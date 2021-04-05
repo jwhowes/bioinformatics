35109d3660875a0fd67ef4c1e3c2fa8b35d8af38
jwhowes <jacob.w.howes@durham.ac.uk>
2021-04-05 14:59:08 +0100

    fixed it I guess

--- EM.py
+++ EM.py
@@ -1,15 +1,11 @@
 import numpy as np
 
+# This whole thing doesn't work
+
 # TODO:
-	# Make lambda be initialised properly (instead of all 0s)
-	# Get it to loop (currently it only does one iteration), need to start thinking about
+	# Something is wrong with my implementation of Viterbi's
+		# But I'm fairly sure (logically) that alpha[L - 1].sum() is the value I'm looking for anyway (ofc at the end of an iteration)
 	# Try researching Baum-Welch intialisation
-	# If we start from all 1s then we end up with something pretty good (as in probability > 0 for L = 1000)
-		# Consider this as an option (could go into some detail about why it was chosen in the report)
-			# i.e. traditionally, Baum-Welch begins from a random set of parameters but in this case, with larger L, the initial probability is too small to even be represented in floating point (so it is truncated to 0). Beginning from 0 probability, the algorithm cannot make any improvements
-			# Although all 1s is not a valid Markov model, it does give the algorithm a starting point from which it is able to build a valid model which produces the sequence with high probability
-			# 10^-300 may sound miniscule but considering that the distribution used to generate X in this case is uniform and (1/4)^1000 ~ 1.14 * 10^-600 it's actually really good
-		# For X = all 1s this gives us probability of 0.991 even for L = 1000
 	# Test it
 		# Try testing it by building a markov model (randomly) then generating a sequence using that.
 			# Probably generating them randomly is what's messing it up (consider probabilities of specific uniformly random sequences)
@@ -28,22 +24,36 @@ def viterbis():
 			v[l][i] = e[l][X[i]] * np.array([v[s][i - 1] * m[s][l] for s in range(k)]).max()
 	return np.array([v[l][L - 1] for l in range(k)]).max()
 
-k = 10
-L = 1000
-alphabet_size = 4
+k = 15
+L = 100
+alphabet_size = 10
 
-X = np.random.randint(0, alphabet_size / 2, (L), dtype=int)
-#X = np.zeros((L), dtype=int)
+epsilon = 1.00001
 
-pi = np.ones((k))
-m = np.ones((k,k))
-e = np.ones((k,alphabet_size))
+#X = np.random.randint(0, alphabet_size, (L), dtype=int)
+X = np.array([i % alphabet_size for i in range(L)])
+
+#pi = np.ones((k))
+#m = np.ones((k,k))
+#e = np.ones((k,alphabet_size))
+
+'''pi = np.array([1, 0, 0, 0], dtype=float)
+
+m = np.array([	[0, 1, 0, 0],
+				[0, 0, 1, 0],
+				[0, 0, 0, 1],
+				[1, 0, 0, 0]], dtype=float)
+
+e = np.array([	[1, 0, 0, 0],
+				[0, 1, 0, 0],
+				[0, 0, 1, 0],
+				[0, 0, 0, 1]], dtype=float)'''
 
 '''pi = np.full((k), 1/k)
 m = np.full((k,k), 1/k)
 e = np.full((k,alphabet_size), 1/alphabet_size)'''
 
-'''pi = np.random.uniform(0, 1, (k))
+pi = np.random.uniform(0, 1, (k))
 pi_s = pi.sum()
 for i in range(k):
 	pi[i] /= pi_s
@@ -58,12 +68,12 @@ e = np.random.uniform(0, 1, (k,alphabet_size))
 for i in range(k):
 	s = e[i].sum()
 	for j in range(alphabet_size):
-		e[i][j] /= s'''
-
-print(viterbis())
-
-for x in range(25):
-	print(x)
+		e[i][j] /= s
+t = 0
+p = 0
+while True:
+	t += 1
+	print(t, p)
 	alpha = np.zeros((L,k))
 	beta = np.zeros((L,k))
 	gamma = np.zeros((L,k))
@@ -75,21 +85,19 @@ for x in range(25):
 	for i in range(k):
 		alpha[0][i] = pi[i] * e[i][X[0]]
 		beta[L - 1][i] = 1
-
-	for i in range(k):
-		for l in range(1,L):
+	for l in range(1, L):
+		for i in range(k):
 			s = 0
 			for j in range(k):
 				s += alpha[l - 1][j] * m[j][i]
 			alpha[l][i] = e[i][X[l]] * s
-		for l in range(L - 2, -1, -1):
+	for l in range(L - 2, -1, -1):
+		for i in range(k):
 			s = 0
 			for j in range(k):
 				s += beta[l+1][j] * m[i][j] * e[j][X[l+1]]
 			beta[l][i] = s
-
 	gamma = alpha * beta
-
 	for l in range(L):
 		s = gamma[l].sum()
 		if s == 0:
@@ -129,5 +137,9 @@ for x in range(25):
 				e[i][b] = s2 / s
 	for i in range(k):
 		m[i] /= m[i].sum()
+	new_p = alpha[L - 1].sum()
+	if p > 0 and new_p / p <= epsilon:
+		break
+	p = new_p
 
-print(viterbis())
+print(alpha[L - 1].sum())
